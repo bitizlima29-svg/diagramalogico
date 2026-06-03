8d1105a74f556007e16f6e73e07aa8b12dbdd008
Ana Beatriz Ferreira Lima <bitizlima.29@gmail.com>
2026-06-03 10:44:19 -0300

    Add files via upload

---
+++ atividade24.sql
@@ -0,0 +1,10 @@
+use db_escola2;
+desc curso;
+insert into curso (nome_curso, carga_horaria) values 
+('banco de dados', 80),
+('programação web',120),
+('lógica da programação',60),
+('desenvolvimento mobile',100);
+
+
+
