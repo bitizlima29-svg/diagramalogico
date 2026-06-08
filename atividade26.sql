10377a64e49808232b0ea3586a58efd9e9e8e2fd
Ana Beatriz Ferreira Lima <bitizlima.29@gmail.com>
2026-06-08 09:47:21 -0300

    Add files via upload

---
+++ atividade26.sql
@@ -0,0 +1,12 @@
+use db_loja;
+CREATE TABLE Cliente (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL
+);
+INSERT INTO Cliente (nome, telefone)
+VALUES
+('Carlos Silva', '(61)9999-90001'),
+('Fernanda Souza', '(61)9999-90007'),
+('Juliana Oliveira', '(61)9999-90009');
+select * from  cliente;
